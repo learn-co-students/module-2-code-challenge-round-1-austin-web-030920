bba204c9d66f319c1765d34850813c56cf1607de
J-Fisk87 <Jeff.Fisk512@gmail.com>
2020-04-09 15:07:09 -0500

    submission for deadline

--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,7 @@
 class Heroine < ApplicationRecord
+    has_many :powers
+    validates :name, presence: true
+    validates :super_name, presence: true, uniqueness: true
+    validates :power_id, presence: true
+
 end
