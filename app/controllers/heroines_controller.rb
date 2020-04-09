01cbd098183ef8dcec4d768c9418025c55b55567
github username <github email address>
2020-04-09 15:08:32 -0500

    Submission for deadline

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,37 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.new
+    @heroine.name = params[:heroine][:name]
+    @heroine.super_name = params[:heroine][:super_name]
+    @heroine.powers << Power.find_by(name: params[:heroine][:powers])
+
+    if @heroine.save
+      redirect_to heroine_path(@heroine)
+    else
+      render :new
+    end
+
+  end
+
+  def edit
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def update
+    @heroine = Heroine.find(params[:id])
+    @heroine.update(params.require(:heroine).permit(:name, :super_name))
+
+    redirect_to heroine_path(@heroine)
+  end
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -2,4 +2,37 @@ class PowersController < ApplicationController
   def index
     @powers = Power.all
   end
+
+  def show
+    @power = Power.find(params[:id])
+  end
+
+  def new
+    @power = Power.new
+  end
+
+  def create
+    @power = Power.new
+    @power.name = params[:power][:name]
+    @power.description = params[:power][:description]
+    @power.save
+
+    redirect_to power_path(@power)
+  end
+
+  def edit
+    @power = Power.find(params[:id])
+  end
+
+  def update
+    @power = Power.find(params[:id])
+
+    changes = params.require(:power).permit(:name, :description, :heroines => [])
+    changes[:heroines].shift
+    heros = changes[:heroines].map do |heroine| heroine = Heroine.find(heroine.to_i) end
+    @power.heroines << heros
+    
+    @power.update(params.require(:power).permit(:name, :description))
+    redirect_to power_path(@power)
+  end
 end
