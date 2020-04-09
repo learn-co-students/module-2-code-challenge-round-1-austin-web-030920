5fddda2e7394c79712836583d90fd662589211fd
Patrick Jones <patrickjones.pmj@gmail.com>
2020-04-09 14:54:32 -0500

    quick save

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,4 +23,12 @@ heroines = [
   {name: "Elektra Natchios", super_name: "Elektra" }
 ]
 
-heroines.each { |heroine| Heroine.create(heroine) }
+powerless_heroines = heroines.map { |heroine| Heroine.create(heroine) }
+powerless_heroines.each do |heroine| 
+  rand(1..2).times do
+    available_powers = Power.all - heroine.powers
+    heroine.powers << available_powers[rand(0..available_powers.count)]
+    binding.pry
+  end
+end
+
