c0de2363e0ab28dabb66aecaa3c5776db569066e
Josh-Gotro <joshuagauthreaux@gmail.com>
2020-04-09 15:07:12 -0500

    submission for deadline

--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,7 @@
 class Heroine < ApplicationRecord
+    has_many :heroine_powers
+    has_many :powers, through: :heroin_powers
+	validates :name, uniqueness: true
+	validates :super_name, uniqueness: true
+
 end
