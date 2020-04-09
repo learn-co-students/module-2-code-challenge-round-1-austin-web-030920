5488eeea0a33060f29039db789ee442c3932cd13
Tr1ckC0 <trick.co@yahoo.com>
2020-04-09 13:10:46 -0500

    migrated HeroinePowers

--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,4 @@
 class Heroine < ApplicationRecord
+    has_many :heroinepowers
+    has_many :heroines, through: :powers
 end
---
+++ test/models/heroine_power_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class HeroinePowerTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+
+  
+end
